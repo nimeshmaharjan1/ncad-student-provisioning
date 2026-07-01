09fe549b54c78c1397f0b2e1f112479fbecf1cdf
Nimesh <maharjann@staff.ncad.ie>
2026-07-01 14:14:08 +0100

    feat: add rewrites to next.config.ts for API integration proxying

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL!;
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 const nextConfig: NextConfig = {
   async rewrites() {
